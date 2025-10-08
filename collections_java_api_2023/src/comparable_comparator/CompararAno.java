02ebe42d83d641691402b8af347b261fbebf248f
maurolqz@gmail.com <maurolqz@gmail.com>
2025-10-08 19:51:21 -0300

    feat:add classes de generics/comparator/comparable-08/10/25-19:50

---
+++ collections_java_api_2023/src/comparable_comparator/CompararAno.java
@@ -0,0 +1,17 @@
+package comparable_comparator;
+
+import java.util.Comparator;
+
+//Classe para comparar Livro por ano
+class CompararAno implements Comparator<Livro> {
+@Override
+public int compare(Livro l1, Livro l2) {
+		if (l1.getAno() < l2.getAno())
+			return -1;
+		if (l1.getAno() > l2.getAno())
+			return 1;
+		else
+			return 0;
+	}
+}
+
