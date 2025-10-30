600c0a78bf7804aa8b62630a81c86017ed29703f
maurolqz@gmail.com <maurolqz@gmail.com>
2025-10-30 09:27:22 -0300

    feat:add classes CadastroProdutos/Produto.30/10/25-09:30

--- collections_java_api_2023/src/set/ordenacao/CadastroProdutos.java
+++ collections_java_api_2023/src/set/ordenacao/CadastroProdutos.java
@@ -1,9 +1,56 @@
 package set.ordenacao;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* classe que possui um conjunto de objetos do tipo "Produto" como atributo.*/
 public class CadastroProdutos {
 
+	Set<Produto> produtoSet;
+	
 	public CadastroProdutos() {
-		// TODO Auto-generated constructor stub
+		this.produtoSet = new HashSet<>();
+	}
+	//Adiciona um produto ao cadastro.
+	public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+		this.produtoSet.add(new Produto(cod, nome, preco, quantidade));
+	}
+	//Exibe todos os produtos do cadastro em ordem alfabética pelo nome.
+	public Set<Produto> exibirProdutosPorNome() {
+		Set<Produto> produtoPorNome = new TreeSet<>(produtoSet);
+		return produtoPorNome;
+	}
+	//Exibe todos os produtos do cadastro em ordem crescente de preço.
+	public Set<Produto> exibirProdutosPorPreco() {
+		Set<Produto> produtoPorPreco = new TreeSet<>(new CompararPorPreco());
+		produtoPorPreco.addAll(produtoSet);
+		return produtoPorPreco;
 	}
 
+	public static void main(String[] args) {
+		// Criando uma instância do CadastroProdutos
+	    CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+	    System.out.println("Adicionando Produtos:");
+	    
+	    // Adicionando produtos ao cadastro
+	    cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+	    cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+	    cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+	    cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+	    System.out.println("Exibindo Produtos:");
+	    System.out.println("------------------");
+	    // Exibindo todos os produtos no cadastro
+	    System.out.println(cadastroProdutos.produtoSet);
+	    System.out.println("Exibindo Produtos Por Nome:");
+	    System.out.println("---------------------------");
+	    // Exibindo produtos ordenados por nome
+	    System.out.println(cadastroProdutos.exibirProdutosPorNome());
+	    System.out.println("Exibindo Produtos Por Preco:");
+	    System.out.println("----------------------------");
+	    // Exibindo produtos ordenados por preço
+	    System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+		
+	}
 }
--- collections_java_api_2023/src/set/ordenacao/Produto.java
+++ collections_java_api_2023/src/set/ordenacao/Produto.java
@@ -1,9 +1,72 @@
 package set.ordenacao;
 
-public class Produto {
+import java.util.Comparator;
+import java.util.Objects;
 
-	public Produto() {
-		// TODO Auto-generated constructor stub
+/*Cada produto possui atributos como nome, cod, preço e quantidade.*/
+public class Produto implements Comparable<Produto> {
+
+	private String nome;
+	private long codigo;
+	private double preco;
+	private int quantidade;
+
+	public Produto(long codigo, String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int compareTo(Produto p) {
+		return nome.compareToIgnoreCase(p.getNome());
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [nome:"+nome+", codigo:"+codigo+", preco:"+preco+", quantidade:"+quantidade+"]";
+	}
+
+}
+
+class CompararPorPreco implements Comparator<Produto> {
+
+	@Override
+	public int compare(Produto p1, Produto p2) {
+		return Double.compare(p1.getPreco(), p2.getPreco());
 	}
 
 }
